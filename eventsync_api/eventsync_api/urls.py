46eed1f1fe52798af22ca3448855e0ec328c8b8d
mts-lucas <lmateus1067@outlook.com>
2024-04-06 19:43:52 -0300

    feat: add api app #20

--- eventsync_api/eventsync_api/urls.py
+++ eventsync_api/eventsync_api/urls.py
@@ -17,10 +17,15 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('eventsync/admin/', admin.site.urls),
+    path('eventsync/api/v1/', include('api.urls')),
+    path('eventsync/', RedirectView.as_view(url='api/v1/')),
+    path('eventsync/api/', RedirectView.as_view(url='v1/')),
+    path('', RedirectView.as_view(url='eventsync/api/v1/')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
