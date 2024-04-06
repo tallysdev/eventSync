19627e24887614e36b14d32f41cfe7b22d626d5a
mts-lucas <lmateus1067@outlook.com>
2024-04-06 18:40:22 -0300

    docs: add readme do back end #20

--- eventsync_api/eventsync_api/settings.py
+++ eventsync_api/eventsync_api/settings.py
@@ -23,7 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-config('SECRET_KEY')
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
