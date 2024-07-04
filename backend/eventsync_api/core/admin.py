1ccc92a59b5f2d102d5ef544c9719246f9cb6b88
mts-lucas <lmateus1067@outlook.com>
2024-07-04 19:40:37 -0300

    feat: add admin #33

--- backend/eventsync_api/core/admin.py
+++ backend/eventsync_api/core/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
+from .models import ESUser
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 
-# Register your models here.
+class ESUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = ESUser
+    list_display = ('email', 'name', 'is_staff', 'is_active', 'user_type')
+    list_filter = ('is_staff', 'is_active', 'user_type')
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal info'), {'fields': ('name', 'cpf', 'birth_date', 'phone', 'user_type')}),
+        (_('Permissions'), {'fields': ('is_staff', 'is_active', 'is_superuser', 'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'name', 'cpf', 'birth_date', 'phone', 'user_type', 'is_staff', 'is_active')}
+        ),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+
+admin.site.register(ESUser, ESUserAdmin)
