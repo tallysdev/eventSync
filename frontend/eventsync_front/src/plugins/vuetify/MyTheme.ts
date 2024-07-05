39a77377f4df11ac8c1160f371fea307e4be80db
Vinicius Maia <viniciusmaiamarinho1@gmail.com>
2024-07-05 16:18:56 -0300

    feat: add base theme #40

---
+++ frontend/eventsync_front/src/plugins/vuetify/MyTheme.ts
@@ -0,0 +1,19 @@
+import type { ThemeDefinition } from 'vuetify'
+
+export const myTheme: ThemeDefinition = {
+  dark: true,
+  colors: {
+    background: '#2F3640',
+    surface: '#2F3640',
+    primary: '#3B5998',
+    secondary: '#2F3640',
+    tertiary: '#FFFFFF',
+    complementary: '#C6D8F1',
+    'primary-darken-1': '#2A4061',
+    'secondary-darken-1': '#1C252B',
+    error: '#B00020',
+    info: '#2196F3',
+    success: '#4CAF50',
+    warning: '#FB8C00'
+  }
+}
