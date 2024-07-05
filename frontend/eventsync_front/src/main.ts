6d6952543cdce58ad48093e935c7f0e135b5cb1d
Vinicius Maia <viniciusmaiamarinho1@gmail.com>
2024-07-05 16:19:42 -0300

    feat: add project config #40
    icon, theme and pinia config

--- frontend/eventsync_front/src/main.ts
+++ frontend/eventsync_front/src/main.ts
@@ -12,15 +12,37 @@ import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
+import { fa } from 'vuetify/iconsets/fa'
+import '@mdi/font/css/materialdesignicons.css'
+import '@fortawesome/fontawesome-free/css/all.css'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+
+import { myTheme } from './plugins/vuetify/MyTheme'
+
 const vuetify = createVuetify({
   components,
   directives,
+  icons: {
+    defaultSet: 'mdi',
+    sets: {
+      fa
+    }
+  },
+  theme: {
+    defaultTheme: 'myTheme',
+    themes: {
+      myTheme
+    }
+  }
 })
 
 const app = createApp(App)
 app.use(vuetify)
 
-app.use(createPinia())
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+// Use the plugin
+app.use(pinia)
 app.use(router)
 
 app.mount('#app')
