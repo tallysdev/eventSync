09e1f60565eb3db40883790b65a89321659a5ca3
mts-lucas <lmateus1067@outlook.com>
2024-07-05 16:23:49 -0300

    chore: add endpoints in main endpoint #33

--- backend/eventsync_api/api/views/root_view.py
+++ backend/eventsync_api/api/views/root_view.py
@@ -1,12 +1,18 @@
 from rest_framework.response import Response
+from rest_framework.reverse import reverse
 from rest_framework.views import APIView
+from core.models import ESUser
 
 
 class ApiRootView(APIView):
     def get(self, request, format=None):
         data = {
-            # endpoint principal para listagem dos demais endpoints
-            "message": "api main view",
+            'users-list': reverse('user_list', request=request, format=format),
+            'user-detail': reverse('user_detail', request=request, format=format),
+            'register': reverse('register', request=request, format=format),
+            'login-token': reverse('token_obtain_pair', request=request, format=format),
+            'refresh-token': reverse('token_refresh', request=request, format=format),
+            # adicione mais endpoints aqui
         }
         return Response(data)
     
