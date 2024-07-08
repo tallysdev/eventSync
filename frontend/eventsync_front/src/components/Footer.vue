8145734d6e5d699b9a6165ca9d7406440bdebee6
Vinicius Maia <viniciusmaiamarinho1@gmail.com>
2024-07-08 10:59:05 -0300

    refactor: update footer style #40

--- frontend/eventsync_front/src/components/Footer.vue
+++ frontend/eventsync_front/src/components/Footer.vue
@@ -1,11 +1,11 @@
 <template>
   <v-footer color="secondary" class="py-6" width="auto" height="auto">
     <v-container>
-      <v-row>
-        <v-col cols="2" class="d-flex align-center">
+      <v-row class="footer-row" align="center" justify="space-between">
+        <v-col cols="12" md="2" class="d-flex align-center justify-center mb-4 mb-md-0">
           <v-img src="../../favicon.ico" alt="Platform Logo" height="40"></v-img>
         </v-col>
-        <v-col cols="3">
+        <v-col cols="12" md="3" class="text-center text-md-left">
           <h3 class="text-h6 font-weight-bold">Encontre Eventos</h3>
           <v-list dense>
             <v-list-item v-for="item in findEventsItems" :key="item">
@@ -13,7 +13,7 @@
             </v-list-item>
           </v-list>
         </v-col>
-        <v-col cols="3">
+        <v-col cols="12" md="3" class="text-center text-md-left">
           <h3 class="text-h6 font-weight-bold">Mapa do site</h3>
           <v-list dense>
             <v-list-item v-for="item in siteMapItems" :key="item">
@@ -21,7 +21,7 @@
             </v-list-item>
           </v-list>
         </v-col>
-        <v-col cols="4">
+        <v-col cols="12" md="4" class="text-center text-md-left">
           <h3 class="text-h6 font-weight-bold">Contato</h3>
           <v-list dense>
             <v-list-item v-for="item in contactItems" :key="item">
@@ -41,5 +41,21 @@ const contactItems = ['Email: fakeemail@gmail.com', 'Celular: 213214124124']
 </script>
 
 <style scoped>
-/* Add any additional styles if needed */
+.footer-row {
+  display: flex;
+  flex-direction: column;
+}
+
+.footer-row > .v-col {
+  margin-bottom: 16px;
+}
+
+@media (min-width: 960px) {
+  .footer-row {
+    flex-direction: row;
+  }
+  .footer-row > .v-col {
+    margin-bottom: 0;
+  }
+}
 </style>
