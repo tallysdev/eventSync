21dd7131a66b984951a6478a6694a6b6bb40f04e
Vinicius Maia <viniciusmaiamarinho1@gmail.com>
2024-07-08 11:04:31 -0300

    refactor: update style #40

--- frontend/eventsync_front/src/views/HomeView.vue
+++ frontend/eventsync_front/src/views/HomeView.vue
@@ -3,10 +3,13 @@ import HomeVue from '@/components/Home.vue';
 </script>
 
 <template>
-  <main class="d-flex align-center justify-center">
+  <main class="d-flex align-center justify-center flex-column h-100 w-100">
     <HomeVue />
   </main>
 </template>
 
 <style scoped>
+main {
+  flex-grow: 1;
+}
 </style>
