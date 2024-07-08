cb80233944f0c19f2fce3cee3b293ad6c93ca808
Vinicius Maia <viniciusmaiamarinho1@gmail.com>
2024-07-08 09:18:13 -0300

    chore: update home view #40

--- frontend/eventsync_front/src/views/HomeView.vue
+++ frontend/eventsync_front/src/views/HomeView.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import HomeVue from '@/components/Home.vue';
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
+  <main class="d-flex align-center justify-center">
+    <HomeVue />
   </main>
 </template>
+
+<style scoped>
+</style>
