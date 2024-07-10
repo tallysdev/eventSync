6df55d37784040b6aff4c511270853c43736c078
mts-lucas <lmateus1067@outlook.com>
2024-07-10 16:59:14 -0300

    fix: add autoschema in settings #46

--- backend/eventsync_api/eventsync_api/settings.py
+++ backend/eventsync_api/eventsync_api/settings.py
@@ -143,6 +143,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
 
 SIMPLE_JWT = {
