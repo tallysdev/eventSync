5245c01dbed32fadd19d33449c7bc9fad7e4ae59
italo <italomauricio98@gmail.com>
2024-07-16 13:49:42 -0300

    feat: add initial event page view, and navbar route #41

---
+++ frontend/eventsync_front/src/views/CreateEventPage.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+      <h1>Crie seu evento</h1>
+      <!-- Adicione seu formulário de criação de evento aqui -->
+    </div>
+  </template>
+  
+  <script setup>
+  </script>
+  
+  <style scoped>
+  /* Adicione seus estilos aqui */
+  </style>
+  
