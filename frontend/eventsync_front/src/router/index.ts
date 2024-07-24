5a7a10158f9d49f74f58615bbcc5ee34fdae84c2
DayXL <day.luc.xav0606@gmail.com>
2024-07-24 10:35:09 -0300

    feat: Add CreateForm view and route #43

--- frontend/eventsync_front/src/router/index.ts
+++ frontend/eventsync_front/src/router/index.ts
@@ -16,6 +16,11 @@ const router = createRouter({
       component: () => import('../views/CreateEventPage.vue')
     },
     {
+      path: '/create-form',
+      name: 'create-form',
+      component: () => import('../views/CreateForm.vue')
+    },
+    {
       path: '/about',
       name: 'about',
       // route level code-splitting
---
+++ frontend/eventsync_front/src/views/CreateForm.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="create-forms">
+      <h1>Criar formulário</h1>
+    </div>
+  </template>
+  
+<style>
+@media (min-width: 1024px) {
+    .forms {
+      min-height: 100vh;
+      display: flex;
+      align-items: center;
+    }
+}
+</style>
+  
