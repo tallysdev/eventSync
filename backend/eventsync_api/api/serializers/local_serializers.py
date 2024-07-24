dc24c59bae7608812628bf3160608b670980add3
mts-lucas <lmateus1067@outlook.com>
2024-07-24 17:26:01 -0300

    feat: add local zerializer #57

---
+++ backend/eventsync_api/api/serializers/local_serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from core.models import Local
+
+class LocalSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Local
+        fields = ['local_name', 'street_name', 'street_number', 'city', 'state', 'cep', 'reference']
+
+        extra_kwargs = {
+            "reference": {"required": False},
+        }
