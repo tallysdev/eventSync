ed1b744360ee2e8918b98980d2fa9081a98690d6
mts-lucas <lmateus1067@outlook.com>
2024-07-29 17:48:32 -0300

    feat: add events tests #57

--- backend/eventsync_api/api/serializers/event_serializers.py
+++ backend/eventsync_api/api/serializers/event_serializers.py
@@ -1,9 +1,7 @@
 from rest_framework import serializers
 from core.models import Event
-from .local_serializers import LocalSerializer
 
 class EventSerializer(serializers.ModelSerializer):
-    local = LocalSerializer()
 
     class Meta:
         model = Event
