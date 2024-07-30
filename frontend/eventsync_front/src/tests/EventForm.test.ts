94df79b03f6772ccbba996a2062286b6327de9c1
DayXL <day.luc.xav0606@gmail.com>
2024-07-29 21:26:43 -0300

    test: Update EventForm component to render components properly #43

--- frontend/eventsync_front/src/tests/EventForm.test.ts
+++ frontend/eventsync_front/src/tests/EventForm.test.ts
@@ -9,9 +9,18 @@ describe('EventForm.vue', () => {
         expect(wrapper.html()).toMatchSnapshot()
     })
 
-    it('renders the component properly', () => {
+    it('renders the components properly', () => {
         expect(wrapper.find('h1.form-title').text()).toBe('Criar formulário')
+        expect(wrapper.find('h2.form-title').text()).toBe('Visualizar Formulário')
+
+    })
+
+    it('contains v-select component for question type', () => {
+        expect(wrapper.find('v-select').exists()).toBe(true)
+    })
+
+    it('contains v-text-field component for question title', () => {
+        expect(wrapper.find('v-text-field').exists()).toBe(true)
     })
     
-  
 })
