6caa644aa1e5d1c202da2e024bc52626eea655d0
Vinicius Maia <viniciusmaiamarinho1@gmail.com>
2024-07-31 19:22:04 -0300

    refactor: removing sponsor form #52

--- frontend/eventsync_front/src/components/SponsorVue.vue
+++ frontend/eventsync_front/src/components/SponsorVue.vue
@@ -5,10 +5,6 @@
       <v-container fluid class="pa-4 d-flex flex-column h-100 w-100">
         <section class="d-block my-10">
           <SponsorList />
-          <v-btn @click="openDialog" color="primary" class="mt-4">
-            <b> Adicionar Patrocinador </b>
-          </v-btn>
-          <SponsorForm :dialog="dialog" @update:dialog="dialog = $event" />
         </section>
       </v-container>
     </v-main>
@@ -17,17 +13,9 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
 import NavBar from './NavBar.vue'
 import FooterVue from './Footer.vue'
 import SponsorList from './sponsors/SponsorList.vue'
-import SponsorForm from './sponsors/SponsorForm.vue'
-
-const dialog = ref(false)
-
-const openDialog = () => {
-  dialog.value = true
-}
 </script>
 
 <style scoped></style>
