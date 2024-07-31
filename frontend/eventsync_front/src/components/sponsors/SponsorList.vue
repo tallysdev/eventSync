29062a32c505742b13c29b27f25e95169635d4a0
Vinicius Maia <viniciusmaiamarinho1@gmail.com>
2024-07-31 19:21:46 -0300

    feat: add error handler #52
    and loading logic plus pagination

--- frontend/eventsync_front/src/components/sponsors/SponsorList.vue
+++ frontend/eventsync_front/src/components/sponsors/SponsorList.vue
@@ -2,9 +2,12 @@
   <v-row justify="center">
     <v-col cols="12" lg="10">
       <h2 class="text-h4 text-start mb-6 font-weight-bold">Patrocinadores</h2>
-      <template v-if="sponsors.length === 0">
+      <template v-if="loading">
+        <v-progress-circular indeterminate color="primary"></v-progress-circular>
+      </template>
+      <template v-else-if="sponsors.length === 0">
         <v-alert type="info" color="info" variant="tonal">
-          <b> Nenhum patrocinador encontrado </b>
+          <b>Nenhum patrocinador encontrado</b>
         </v-alert>
       </template>
       <v-row v-else>
@@ -19,29 +22,63 @@
           </v-card>
         </v-col>
       </v-row>
+      <v-row justify="center" class="mt-4">
+        <v-col cols="auto">
+          <v-pagination
+            v-model="currentPage"
+            :length="totalPages"
+            @input="fetchSponsors"
+          ></v-pagination>
+        </v-col>
+      </v-row>
+      <v-btn @click="openDialog" color="primary" class="mt-4">
+        <b> Adicionar Patrocinador </b>
+      </v-btn>
+      <SponsorForm :dialog="dialog" @update:dialog="dialog = $event" />
+      <v-alert v-if="errorMessage" type="error" dismissible>{{ errorMessage }}</v-alert>
     </v-col>
   </v-row>
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted } from 'vue'
-import api from '@/services/api'
+import { fetchSponsors } from '@/services/sponsorService'
+import { ref, onMounted, watch } from 'vue'
 import { type Sponsor } from '@/types/sponsor'
+import SponsorForm from './SponsorForm.vue'
 
 const sponsors = ref<Sponsor[]>([])
+const currentPage = ref(1)
+const totalPages = ref(1)
+const itemsPerPage = 6
+const dialog = ref(false)
+const loading = ref(false)
+const errorMessage = ref<string | null>(null)
 
-const fetchSponsors = async () => {
+const openDialog = () => {
+  dialog.value = true
+}
+
+const fetchSponsorsData = async () => {
+  loading.value = true
+  errorMessage.value = null
   try {
-    const response = await api.get('sponsors/')
+    const response = await fetchSponsors(currentPage.value, itemsPerPage)
     sponsors.value = response.data.results
-    console.log(sponsors.value)
+    totalPages.value = Math.ceil(response.data.count / itemsPerPage)
   } catch (error) {
-    console.error(error)
+    console.error('Error fetching sponsors:', error)
+    errorMessage.value =
+      'Ocorreu um erro ao tentar buscar os patrocinadores. Por favor, tente novamente.'
   }
+  loading.value = false
 }
 
 onMounted(() => {
-  fetchSponsors()
+  fetchSponsorsData()
+})
+
+watch(currentPage, () => {
+  fetchSponsorsData()
 })
 </script>
 
