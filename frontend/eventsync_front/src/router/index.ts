c6c13a9860156ec015545cc3e8a42be788dddeb3
Tallys-Aureliano <tallysaureliano@outlook.com>
2024-07-30 21:17:16 -0300

    :sparkles:feat: theme room router #42

--- frontend/eventsync_front/src/router/index.ts
+++ frontend/eventsync_front/src/router/index.ts
@@ -27,6 +27,11 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/events/:id/theme-room',
+      name: 'theme-room',
+      component: () => import('../views/ThemeRoomView.vue')
     }
   ]
 })
