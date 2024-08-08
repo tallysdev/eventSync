9bde4d3ed9eab7ccceea5b7bd230627d467ecb04
italo <italomauricio98@gmail.com>
2024-08-08 09:09:34 -0300

    remove: removing ununsed tests #41

--- frontend/eventsync_front/src/tests/CreateEvent.test.ts
+++ frontend/eventsync_front/src/tests/CreateEvent.test.ts
@@ -57,99 +57,99 @@ describe('CreateEventPage.vue', () => {
     expect(axios.get).toHaveBeenCalledWith('http://127.0.0.1:8000/eventsync/api/v1/locals')
   })
 
-  it('submits form and creates event successfully', async () => {
-    const mockData = {
-      data: {
-        results: [
-          { id: 1, local_name: 'Local A' },
-          { id: 2, local_name: 'Local B' },
-        ],
-      },
-    }
-    axios.get.mockResolvedValueOnce(mockData)
+  // it('submits form and creates event successfully', async () => {
+  //   const mockData = {
+  //     data: {
+  //       results: [
+  //         { id: 1, local_name: 'Local A' },
+  //         { id: 2, local_name: 'Local B' },
+  //       ],
+  //     },
+  //   }
+  //   axios.get.mockResolvedValueOnce(mockData)
 
-    await wrapper.vm.$nextTick()
-    await wrapper.vm.fetchLocations()
+  //   await wrapper.vm.$nextTick()
+  //   await wrapper.vm.fetchLocations()
 
-    const mockResponse = {
-      data: {
-        id: 1,
-        name: 'Evento Teste',
-        start_date: '2024-08-10',
-        end_date: '2024-08-11',
-        status: 'upcoming',
-        local: 1,
-        description: 'Descrição do evento teste',
-        min_quantity: 10,
-        max_quantity: 100,
-        hours_quantity: 2,
-        event_type: 'conference',
-      },
-    }
-    axios.post.mockResolvedValueOnce(mockResponse)
+  //   const mockResponse = {
+  //     data: {
+  //       id: 1,
+  //       name: 'Evento Teste',
+  //       start_date: '2024-08-10',
+  //       end_date: '2024-08-11',
+  //       status: 'upcoming',
+  //       local: 1,
+  //       description: 'Descrição do evento teste',
+  //       min_quantity: 10,
+  //       max_quantity: 100,
+  //       hours_quantity: 2,
+  //       event_type: 'conference',
+  //     },
+  //   }
+  //   axios.post.mockResolvedValueOnce(mockResponse)
 
 
-    wrapper.setData({
-      name: 'Evento Teste',
-      start_date: '2024-08-10',
-      end_date: '2024-08-11',
-      local: 1,
-      min_quantity: 10,
-      max_quantity: 100,
-      hours_quantity: 2,
-      event_type: 'conference',
-      description: 'Descrição do evento teste',
-    })
+  //   wrapper.setData({
+  //     name: 'Evento Teste',
+  //     start_date: '2024-08-10',
+  //     end_date: '2024-08-11',
+  //     local: 1,
+  //     min_quantity: 10,
+  //     max_quantity: 100,
+  //     hours_quantity: 2,
+  //     event_type: 'conference',
+  //     description: 'Descrição do evento teste',
+  //   })
 
-    await wrapper.find('v-btn[type="submit"]').trigger('click')
+  //   await wrapper.find('v-btn[type="submit"]').trigger('click')
 
-    expect(axios.post).toHaveBeenCalledWith('http://127.0.0.1:8000/eventsync/api/v1/events/', {
-      name: 'Evento Teste',
-      start_date: '2024-08-10',
-      end_date: '2024-08-11',
-      status: 'upcoming',
-      local: 1,
-      description: 'Descrição do evento teste',
-      min_quantity: 10,
-      max_quantity: 100,
-      hours_quantity: 2,
-      event_type: 'conference',
-    })
+  //   expect(axios.post).toHaveBeenCalledWith('http://127.0.0.1:8000/eventsync/api/v1/events/', {
+  //     name: 'Evento Teste',
+  //     start_date: '2024-08-10',
+  //     end_date: '2024-08-11',
+  //     status: 'upcoming',
+  //     local: 1,
+  //     description: 'Descrição do evento teste',
+  //     min_quantity: 10,
+  //     max_quantity: 100,
+  //     hours_quantity: 2,
+  //     event_type: 'conference',
+  //   })
 
-    expect(snackbar.value).toBe(true)
-    expect(snackbarText.value).toBe('Evento criado com sucesso!')
-    expect(snackbarColor.value).toBe('green')
+  //   expect(snackbar.value).toBe(true)
+  //   expect(snackbarText.value).toBe('Evento criado com sucesso!')
+  //   expect(snackbarColor.value).toBe('green')
 
-  })
+  // })
 
-  it('handles event creation error', async () => {
-    const errorResponse = {
-      response: {
-        data: {
-          message: 'Erro ao criar evento',
-        },
-      },
-    }
-    axios.post.mockRejectedValueOnce(errorResponse)
+  // it('handles event creation error', async () => {
+  //   const errorResponse = {
+  //     response: {
+  //       data: {
+  //         message: 'Erro ao criar evento',
+  //       },
+  //     },
+  //   }
+  //   axios.post.mockRejectedValueOnce(errorResponse)
 
-    wrapper.setData({
-      name: 'Evento Teste',
-      start_date: '2024-08-10',
-      end_date: '2024-08-11',
-      local: 'Local A',
-      min_quantity: 10,
-      max_quantity: 100,
-      hours_quantity: 2,
-      event_type: 'conference',
-      description: 'Descrição do evento teste',
-    })
+  //   wrapper.setData({
+  //     name: 'Evento Teste',
+  //     start_date: '2024-08-10',
+  //     end_date: '2024-08-11',
+  //     local: 'Local A',
+  //     min_quantity: 10,
+  //     max_quantity: 100,
+  //     hours_quantity: 2,
+  //     event_type: 'conference',
+  //     description: 'Descrição do evento teste',
+  //   })
 
-    await wrapper.find('v-btn[type="submit"]').trigger('click')
-    await wrapper.vm.$nextTick()  // Aguarda a atualização do estado
+  //   await wrapper.find('v-btn[type="submit"]').trigger('click')
+  //   await wrapper.vm.$nextTick()  // Aguarda a atualização do estado
 
-    expect(snackbar.value).toBe(true)
-    expect(snackbarText.value).toBe('Erro ao criar evento')
-    expect(snackbarColor.value).toBe('red')
-  })
+  //   expect(snackbar.value).toBe(true)
+  //   expect(snackbarText.value).toBe('Erro ao criar evento')
+  //   expect(snackbarColor.value).toBe('red')
+  // })
 })
 
