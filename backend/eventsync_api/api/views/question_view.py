a3b9d8dab0c44ea6799338011e37209a2a53d01e
DayXL <day.luc.xav0606@gmail.com>
2024-08-11 15:56:11 -0300

    feat: Add QuestionList and QuestionDetail views #60

---
+++ backend/eventsync_api/api/views/question_view.py
@@ -0,0 +1,11 @@
+from rest_framework import generics
+from core.models import Question
+from ..serializers.question_serializers import QuestionSerializer
+
+class QuestionList(generics.ListCreateAPIView):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
+
+class QuestionDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
