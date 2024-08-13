3bbac28e92af9e10f45686691e1d4dea4d18d5e9
DayXL <day.luc.xav0606@gmail.com>
2024-08-13 10:35:25 -0300

    feat: Add validation for form submission #60

--- backend/eventsync_api/api/views/question_view.py
+++ backend/eventsync_api/api/views/question_view.py
@@ -1,10 +1,19 @@
 from rest_framework import generics
 from core.models import Question
 from ..serializers.question_serializers import QuestionSerializer
-
+from rest_framework.response import Response
+from rest_framework import status
 class QuestionList(generics.ListCreateAPIView):
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer
+    
+    def post(self, request, format=None):
+        serializer = QuestionSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class QuestionDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Question.objects.all()
