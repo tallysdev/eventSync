11d5c31d673408c1cc9351e11ec990cce07d046f
Tallys-Aureliano <tallysaureliano@outlook.com>
2024-08-13 22:33:54 -0300

    feat: new fields #42

--- frontend/eventsync_front/src/components/ThemeRoom.vue
+++ frontend/eventsync_front/src/components/ThemeRoom.vue
@@ -4,26 +4,24 @@
             <h1>Evento: Nome do Evento</h1>
             <h1 class="text-center">Criar sala temática.</h1>
             <v-form class="mb-10">
-                <v-text-field variant="outlined" label="Local da sala"></v-text-field>
-                <v-text-field variant="outlined" label="Nome da sala"></v-text-field>
-                <v-text-field variant="outlined" label="Descrição"></v-text-field>
-                <v-text-field variant="outlined" label="Público Alvo"></v-text-field>
                 <v-text-field variant="outlined" label="Data Inicial" type="date"></v-text-field>
                 <v-text-field variant="outlined" label="Data Final" type="date"></v-text-field>
                 <v-text-field variant="outlined"
-                    name="name"
-                    label="Horario inicial da sala."
-                    id="id"
-                    type="time"
+                name="name"
+                label="Horario inicial da sala."
+                id="id"
+                type="time"
                 ></v-text-field>
-                <v-text-field variant="outlined"
-                    name="name"
-                    label="Horario final da sala."
-                    id="id"
-                    type="time"
-                ></v-text-field>
+                <v-text-field variant="outlined" label="Local da sala"></v-text-field>
                 <v-text-field variant="outlined" label="Número Min. de Participantes"></v-text-field>
                 <v-text-field variant="outlined" label="Número Máx. de Participantes"></v-text-field>
+                <v-text-field variant="outlined" label="Palestrante"></v-text-field>
+                <v-text-field variant="outlined" label="Nome da sala"></v-text-field>
+                <v-text-field variant="outlined" label="Descrição"></v-text-field>
+                <v-text-field variant="outlined" label="Quantidade horas no certificado"></v-text-field>
+                <v-text-field variant="outlined" label="Público Alvo"></v-text-field>
+                <v-text-field variant="outlined" label="Tipo de evento."></v-text-field>
+
                 <v-container grid-list-xs class="d-flex justify-center ga-10">
                     <v-btn color="primary">Criar</v-btn>
                     <v-btn color="secondary">Voltar</v-btn>
@@ -35,13 +33,7 @@
 
 <script>
 export default {
-    data() {
-        return {
-            time: null,
-            menu2: false,
-            modal2: false,
-        }
-    },
+
 }
 </script>
 <style lang="">
