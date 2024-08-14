a8ca674efa10821388649fada7b5bb9ee5a7e750
Vinicius Maia <viniciusmaiamarinho1@gmail.com>
2024-08-14 17:12:37 -0300

    feat: add event view #66

---
+++ frontend/eventsync_front/src/views/EventListView.vue
@@ -0,0 +1,15 @@
+<template>
+  <main class="d-flex align-center justify-center flex-column h-100 w-100">
+    <Event />
+  </main>
+</template>
+
+<script setup lang="ts">
+import Event from '@/components/Event.vue'
+</script>
+
+<style scoped>
+main {
+  flex-grow: 1;
+}
+</style>
