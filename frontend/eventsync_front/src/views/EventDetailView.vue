a045692f0e9e89277f55a07771917717b6672d80
Vinicius Maia <viniciusmaiamarinho1@gmail.com>
2024-08-15 20:39:12 -0300

    feat: add event detail view #66

---
+++ frontend/eventsync_front/src/views/EventDetailView.vue
@@ -0,0 +1,15 @@
+<template>
+  <main class="d-flex align-center justify-center flex-column h-100 w-100">
+    <EventDetail />
+  </main>
+</template>
+
+<script setup lang="ts">
+import EventDetail from '../components/EventDetail.vue'
+</script>
+
+<style scoped>
+main {
+  flex-grow: 1;
+}
+</style>
