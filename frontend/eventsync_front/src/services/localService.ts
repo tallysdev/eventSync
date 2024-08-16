bbfa502edc4c9fe8e35f747df51eff11be730724
italo <italomauricio98@gmail.com>
2024-08-15 21:24:43 -0300

    feat: add update local service #71

--- frontend/eventsync_front/src/services/localService.ts
+++ frontend/eventsync_front/src/services/localService.ts
@@ -1,17 +1,27 @@
 import api from '@/services/api'
+import { type Local } from '@/types/local'
+import axios from 'axios'
 
 export const fetchLocations = (page: number, pageSize: number) => {
-  return api.get('locals/', {
+return api.get('locals/', {
     params: { page, page_size: pageSize }
-  })
+})
 }
 
 export const addLocal = (formData: FormData) => {
-  return api.post('locals/', formData, {
+return api.post('locals/', formData, {
     headers: { 'Content-Type': 'multipart/form-data' }
-  })
+})
 }
 
 export const fetchLocal = () => {
-  return api.get('locals');
+return api.get('locals');
+}
+
+export const updateLocal = async (local: Local) => {
+    await api.put(`locals/${local.id}/`, local)
+}
+
+export const deleteLocal = async (id: number) => {
+    await api.delete(`/api/locals/${id}/`)
 }
