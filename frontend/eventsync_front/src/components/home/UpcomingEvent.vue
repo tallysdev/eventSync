14d0a70da7a3efb14644bb0356ceb1bc00acbde2
Vinicius Maia <viniciusmaiamarinho1@gmail.com>
2024-08-16 18:10:41 -0300

    feat: conect carrousel to api #66

--- frontend/eventsync_front/src/components/home/UpcomingEvent.vue
+++ frontend/eventsync_front/src/components/home/UpcomingEvent.vue
@@ -1,25 +1,51 @@
 <template>
   <section class="d-block">
-    <!-- TODO: Make the event clickable and redirect to the event -->
     <v-row justify="center">
       <v-col cols="12" lg="10">
         <h2 class="text-h4 text-start mb-6 font-weight-bold">Eventos próximos</h2>
-        <v-carousel height="500" show-arrows="hover" class="rounded-xl position-relative">
+        <v-carousel
+          v-if="upcomingEvents.length > 0"
+          height="500"
+          show-arrows="hover"
+          class="rounded-xl position-relative"
+        >
           <v-carousel-item v-for="event in upcomingEvents" :key="event.id">
             <v-row class="no-gutters height93">
               <v-col cols="12" md="8" class="bg-primary d-none d-md-flex">
                 <div class="h-100 w-100"></div>
               </v-col>
               <v-col cols="12" md="4" class="m-0 pa-4 event-details bg-white">
-                <div class="d-flex flex-column align-start justify-flex-start h-100 pl-4">
+                <div class="d-flex flex-column align-start justify-flex-start h-100">
                   <div class="d-flex flex-column h-75 mt-4">
-                    <p class="primary--text text-h6 font-weight-bold">
-                      {{ event.date }} - {{ event.time }}
+                    <div class="d-flex align-center mb-2">
+                      <v-icon class="mr-2" color="background">mdi-calendar</v-icon>
+                      <p class="primary--text text-h6 font-weight-bold">
+                        {{ formatDate(event.start_date) }} - {{ formatDate(event.end_date) }}
+                      </p>
+                    </div>
+                    <h1 class="text-h4 font-weight-bold text-left event-title">
+                      {{ event.name }}
+                    </h1>
+                    <div class="d-flex align-center mb-2">
+                      <v-icon class="mr-2" color="background">mdi-map-marker</v-icon>
+                      <p class="text-h6 font-weight-light primary--text">
+                        {{ event.locationDetails.local_name }},
+                        {{ event.locationDetails.street_name }}
+                        {{ event.locationDetails.street_number }},
+                        {{ event.locationDetails.city }} -
+                        {{ event.locationDetails.state }}
+                      </p>
+                    </div>
+                    <p class="text-h5">
+                      {{ event.description }}
                     </p>
-                    <h1 class="text-h4 font-weight-bold text-left event-title">{{ truncateText(event.name, 90) }}</h1>
-                    <p class="text-h6 font-weight-light" color="primary">{{ truncateText(event.location, 40) }}</p>
                   </div>
-                  <v-btn color="primary black--text mt-14" variant="outlined" dark>
+                  <v-btn
+                    color="primary black--text mt-14"
+                    variant="outlined"
+                    dark
+                    @click="goToEvent(event.id)"
+                  >
                     <b>Ver detalhes</b>
                   </v-btn>
                 </div>
@@ -27,22 +53,56 @@
             </v-row>
           </v-carousel-item>
         </v-carousel>
+        <div v-else class="text-center mt-10">
+          <p class="text-h5 primary--text">Nenhum evento próximo disponível.</p>
+        </div>
       </v-col>
     </v-row>
   </section>
 </template>
 
 <script setup lang="ts">
-defineProps({
-  upcomingEvents: {
-    type: Array,
-    required: true
-  }
-})
-const truncateText = (text: string, length: number): string => {
-  return text.length > length ? text.substring(0, length) + '...' : text;
-};
+import { ref, onMounted } from 'vue'
+import { fetchEvents, fetchLocal } from '@/services/eventService'
+import { useRouter } from 'vue-router'
+import { formatDate } from '@/utils/formatDate'
 
+const router = useRouter()
+const upcomingEvents = ref([])
+
+const fetchLocationDetails = async (localId: number) => {
+  try {
+    const response = await fetchLocal(localId)
+    return response.data
+  } catch (error) {
+    console.error('Error fetching local detail:', error)
+    return null
+  }
+}
+
+const fetchUpcomingEvents = async () => {
+  const response = await fetchEvents(1, 4, 'upcoming')
+  const events = response.data.results
+
+  // Fetch location details for each event
+  for (const event of events) {
+    if (event.local) {
+      event.locationDetails = await fetchLocationDetails(event.local)
+    } else {
+      event.locationDetails = {}
+    }
+  }
+
+  upcomingEvents.value = events
+}
+
+const goToEvent = (id: number): void => {
+  router.push(`/events/${id}`)
+}
+
+onMounted(() => {
+  fetchUpcomingEvents()
+})
 </script>
 
 <style scoped>
@@ -58,7 +118,6 @@ const truncateText = (text: string, length: number): string => {
   width: 100% !important;
   color: white !important;
 }
-
 
 .event-details {
   display: flex;
@@ -86,5 +145,4 @@ const truncateText = (text: string, length: number): string => {
     font-size: 1.5rem !important;
   }
 }
-
 </style>
