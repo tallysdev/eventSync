57d4e856a405b9c6ea913920ccf6fc95c4347ac0
Tallys-Aureliano <tallysaureliano@outlook.com>
2024-08-18 12:53:53 -0300

    feat: event-based rooms #42

--- backend/eventsync_api/api/views/theme_room_view.py
+++ backend/eventsync_api/api/views/theme_room_view.py
@@ -2,7 +2,6 @@ from core.models import ThemeRoom
 from django.http import Http404
 from drf_spectacular.utils import OpenApiParameter, extend_schema
 from rest_framework import status
-from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -13,41 +12,58 @@ from ..serializers.theme_room_serializers import ThemeRoomSerializer
 
 class ThemeRoomListView(APIView):
     """
-    List all Themes Rooms, or create a new Theme Room.
+    List all Theme Rooms for a specific Event, or create a new Theme Room for that Event.
     """
     # permission_classes = [IsAuthenticated | ReadOnly]  
-    pagination_class = PageNumberPagination
 
     @extend_schema(
         responses={200: ThemeRoomSerializer(many=True)},
         parameters=[
+            OpenApiParameter(name='event_id', description='Event ID', required=True, type=int),
             OpenApiParameter(name='page', description='Page number', required=False, type=int),
             OpenApiParameter(name='page_size', description='Page size', required=False, type=int),
         ], 
     )
-
     def get(self, request, format=None):
-        themeRoom = ThemeRoom.objects.all().order_by("id")
-        paginator = self.pagination_class()
-        result_page = paginator.paginate_queryset(themeRoom, request)
-        serializer = ThemeRoomSerializer(result_page, many=True)
-        return paginator.get_paginated_response(serializer.data)
+        # Obtenha o ID do evento da query string
+        event_id = request.query_params.get('event_id')
+        
+        if not event_id:
+            return Response({"detail": "Event ID is required."}, status=status.HTTP_400_BAD_REQUEST)
+        
+        # Filtrar as Theme Rooms pelo evento específico
+        theme_rooms = ThemeRoom.objects.filter(event__id=event_id).order_by("id")
+        
+        # Serializar os dados
+        serializer = ThemeRoomSerializer(theme_rooms, many=True)
+        
+        return Response(serializer.data)
+
     @extend_schema(
         request=ThemeRoomSerializer,
         responses={201: ThemeRoomSerializer},
     )
     def post(self, request, format=None):
-        # print("oi lucas", request.data)
+        # Obtenha o ID do evento da query string ou do corpo da requisição
+        event_id = request.data.get('event_id')
+        
+        if not event_id:
+            return Response({"detail": "Event ID is required."}, status=status.HTTP_400_BAD_REQUEST)
+        
+        # Adicione o evento aos dados de criação
+        request.data['event'] = event_id
         serializer = ThemeRoomSerializer(data=request.data)
+        
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class ThemeRoomDetailView(APIView):
     """
-    Retrieve, update or delete an Theme Room.
+    Retrieve, update or delete a Theme Room.
     """
     permission_classes = [IsAuthenticated | ReadOnly]
 
