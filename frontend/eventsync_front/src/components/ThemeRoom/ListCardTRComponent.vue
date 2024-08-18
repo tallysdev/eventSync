45fa3dbe09c9231bb02e9e308469b33167c59775
Tallys-Aureliano <tallysaureliano@outlook.com>
2024-08-18 12:56:07 -0300

    feat: final form #42

--- frontend/eventsync_front/src/components/ThemeRoom/ListCardTRComponent.vue
+++ frontend/eventsync_front/src/components/ThemeRoom/ListCardTRComponent.vue
@@ -1,89 +1,92 @@
 <template>
-    <v-container fluid class="ma-4 d-flex ga-5 w-100 flex-wrap border-red">
-        <v-card class="mx-auto flex-wrap" variant="outlined" max-width="400" v-for="(room, id) in rooms" :key="id">
-            <v-card-title class="font-weight-bold text-h5">{{ room.name }}</v-card-title>
-
-            <v-card-subtitle class="text-caption grey--text">
-                <v-icon class="mr-1" small>mdi-calendar</v-icon>
-                De {{ room.start_date }} - Até {{ room.end_date }}
-            </v-card-subtitle>
-
-            <v-card-subtitle class="text-caption grey--text">
-                <v-icon class="mr-1" small>mdi-clock-outline</v-icon>
-                Horário de início {{ room.start_time }}
-            </v-card-subtitle>
-
+  <h1 class="text-center">Evento {{ EventId }}</h1>
+  <h1 class="mx-10">Programação:</h1>
+  <v-container fluid class="d-flex flex-column align-center">
+    <v-btn color="success d-flex">Cadastrar Sala Tematica</v-btn>
+    <h2 v-if="rooms.length === 0">Nenhuma Sala Cadastrada ainda...</h2>
+    <v-container fluid class="d-flex flex-wrap ga-5 justify-center">
+    
+      <v-card class="mx-2 my-2 justify-center w-75" variant="outlined" elevation="20" max-width="400" min-width="50"
+      v-for="(room, id) in rooms" :key="id">
+      <v-card-actions v-if="admin" class="d-flex flex-wrap w-100 justify-space-between">
+        <v-btn color="yellow" variant="text"  @click="fetchEditRoom(room.id)">Editar</v-btn>
+        <v-btn color="red" variant="text"  @click="fetchDeleteRoom(room.id)">Exluir</v-btn>
+      </v-card-actions>
+      <v-card-title class="font-weight-bold text-h5 text-center">{{ room.name }}</v-card-title>
+      <v-card-subtitle class="text-caption grey--text">
+        <v-icon class="mr-1" small>mdi-calendar</v-icon>
+        De {{ room.start_date }} - Até {{ room.end_date }}
+      </v-card-subtitle>
+      <v-card-subtitle class="text-caption grey--text">
+        <v-icon class="mr-1" small>mdi-clock-outline</v-icon>
+        Horário de início {{ room.start_time }}
+      </v-card-subtitle>
+      <v-card-subtitle class="text-caption grey--text">
+        <v-icon class="mr-1" small>mdi-map-marker-outline</v-icon>
+        <span class="font-weight-bold">{{ room.local }}</span>
+      </v-card-subtitle>
+      <v-card-text>
+        <v-row class="d-flex flex-column">
+          <v-col>
+            <p class="font-weight-bold">Palestrante: {{ room.speaker }}</p>
+          </v-col>
+          <v-col>
+            <v-icon class="mr-1" small>mdi-information-outline</v-icon>
+            <span class="font-weight-bold">Tipo: </span>
+            {{ room.event_type }}
+          </v-col>
+          <v-col class="d-flex flex-wrap">
+            <v-icon class="mr-1" small>mdi-account-group</v-icon>
+            <p class="font-weight-bold">Participantes:</p>
+            <p>Máximo {{ room.max_quantity }} - Mínimo {{ room.min_quantity }}
+            </p>
+          </v-col>
+        </v-row>
+      </v-card-text>
+      <v-card-actions class="d-flex flex-wrap w-100 justify-center">
+        <v-btn color="blue" variant="text" @click="toggleDetails(id)">Ver Detalhes</v-btn>
+        <v-btn color="green" variant="text">Participar</v-btn>
+      </v-card-actions>
+      <v-expand-transition>
+        <div v-show="room.showDetails">
+          <v-divider></v-divider>
+          <v-row class="pa-2">
             <v-card-text>
-                <v-row>
-                    <v-col cols="12">
-                        <v-icon class="mr-1" small>mdi-map-marker-outline</v-icon>
-                        <span class="font-weight-bold">{{ room.local }}</span>
-                    </v-col>
-
-                    <v-col cols="12">
-                        <v-icon class="mr-1" small>mdi-account-group</v-icon>
-                        <span class="font-weight-bold">Participantes: </span>
-                        Máximo {{ room.max_quantity }} - Mínimo {{ room.min_quantity }}
-                    </v-col>
-
-                    <v-col cols="12">
-                        <v-icon class="mr-1" small>mdi-information-outline</v-icon>
-                        <span class="font-weight-bold">Tipo: </span>
-                        {{ room.event_type }}
-                    </v-col>
-
-                    <v-col cols="12">
-                        <p class="mt-2">{{ room.description }}</p>
-                    </v-col>
-
-                    <v-col cols="12">
-                        <p>Público alvo: {{ room.audiences }}</p>
-                    </v-col>
-
-                    <v-col cols="12">
-                        <p>Palestrante: {{ room.speaker }}</p>
-                    </v-col>
-                </v-row>
+              Público Alvo: {{ room.audiences }}
+              <br>
+              Descrição: {{ room.description }}
             </v-card-text>
-
-            <v-card-actions>
-                <v-btn color="blue" text @click="toggleDetails(id)">Ver Detalhes</v-btn>
-                <v-btn color="green" text>Participar</v-btn>
-            </v-card-actions>
-
-            <v-expand-transition>
-                <div v-show="room.showDetails">
-                    <v-divider></v-divider>
-
-                    <v-card-text>
-                        Detalhes adicionais da Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat neque pariatur veniam, distinctio illo dolores, quisquam molestiae iure voluptates ab consequuntur, quis suscipit autem qui fuga facilis facere aliquid. Molestias. sala. Você pode adicionar mais informações aqui.
-                    </v-card-text>
-                </div>
-            </v-expand-transition>
-        </v-card>
-    </v-container>
+          </v-row>
+        </div>
+      </v-expand-transition>
+    </v-card>
+  </v-container>  
+  </v-container>
 </template>
 
-
 <script setup lang="ts">
-import { ref, onMounted, watch } from 'vue'
-import { fetchThemeRooms } from '@/services/themRoomService';
+import { ref, onMounted} from 'vue'
+import { fetchThemeRooms, deleteThemeRoom } from '@/services/themRoomService';
 import { type ThemeRoom } from '@/types/themeRoom';
+import { useRoute } from 'vue-router';
 
+const route = useRoute()
+const EventId = Number(route.params.id)
 const rooms = ref<ThemeRoom[]>([])
-const currentPage = ref(1)
-const totalPages = ref(1)
-const itemsPerPage = 12
 const loading = ref(false)
 const errorMessage = ref<string | null>(null)
-
+const admin = true
 // Função para alternar o estado 'showDetails' para cada sala individualmente
-const toggleDetails = (id: string) => {
-  const room = rooms.value.find((room: { id: string; }) => room.id === id)
+const toggleDetails = (id: number) => {
+  // Encontra a sala com o id correto
+  const room = rooms.value.find((room: { id: number; }) => room.id === id)
+
+  // Verifica se a sala foi encontrada e alterna o estado de 'showDetails'
   if (room) {
     room.showDetails = !room.showDetails
   }
 }
+
 
 const fetchRoomsData = async () => {
   loading.value = true
@@ -95,13 +98,8 @@ const fetchRoomsData = async () => {
   }, 10000) // 10 seconds timeout
 
   try {
-    const response = await fetchThemeRooms(currentPage.value, itemsPerPage)
-    rooms.value = response.data.results.map((room: ThemeRoom) => ({
-      ...room,
-      showDetails: false, // Adiciona a propriedade showDetails a cada sala
-    }))
-    console.log(rooms.value)
-    totalPages.value = Math.ceil(response.data.count / itemsPerPage)
+    const response = await fetchThemeRooms(EventId)
+    // console.log(rooms.value)
     clearTimeout(loadingTimeout)
   } catch (error) {
     console.error('Error fetching rooms:', error)
@@ -110,13 +108,30 @@ const fetchRoomsData = async () => {
   }
 }
 
+const fetchDeleteRoom = async (id: number) => {
+  loading.value = true
+  errorMessage.value = null
+
+  const loadingTimeout = setTimeout(() => {
+    loading.value = false
+    errorMessage.value = 'O tempo de carregamento expirou. Tente novamente.'
+  }, 10000) // 10 seconds timeout
+
+  try {
+    const response = await deleteThemeRoom(id)
+    clearTimeout(loadingTimeout)
+  } catch (error) {
+    console.error('Error deleting room:', error)
+  } finally {
+    loading.value = false
+    fetchRoomsData()
+  }
+}
+
 onMounted(() => {
   fetchRoomsData()
 })
 
-watch(currentPage, () => {
-  fetchRoomsData()
-})
 </script>
 
 <style scoped lang="css"></style>
