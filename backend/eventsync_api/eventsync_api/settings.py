58f70cec8bac61e67de87f36d50ecaa67a5af79c
mts-lucas <lmateus1067@outlook.com>
2024-08-20 10:56:38 -0300

    chore: dell unused confs #74

--- backend/eventsync_api/eventsync_api/settings.py
+++ backend/eventsync_api/eventsync_api/settings.py
@@ -166,14 +166,3 @@ SPECTACULAR_SETTINGS = {
 
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOW_HEADERS = [
-'accept',
-'accept-encoding',
-'authorization',
-'content-type',
-'dnt',
-'origin',
-'user-agent',
-'x-csrftoken',
-'x-requested-with',
-]
