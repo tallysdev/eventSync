4f10ea783572c63e80e9d3578b3dd8dcc6c13a27
Tallys-Aureliano <tallysaureliano@outlook.com>
2024-08-20 17:46:20 -0300

    style: revert tab #42

--- frontend/eventsync_front/src/components/ThemeRoom/ListRooms.vue
+++ frontend/eventsync_front/src/components/ThemeRoom/ListRooms.vue
@@ -1,77 +1,77 @@
 <template>
-    <v-container fluid class="d-flex flex-column align-center">
-        <v-btn v-if="admin" :to="`/events/${EventId}/create/theme-room`" color="success">Cadastrar Sala Tematica
-        </v-btn>
-        <h2 v-if="rooms.length === 0">Nenhuma Sala Cadastrada ainda...</h2>
-        <v-container fluid class="d-flex flex-wrap ga-5 justify-center">
-            <v-card class="mx-2 my-2 justify-center w-100" variant="outlined" elevation="20" max-width="400"
-                min-width="50" v-for="(room, id) in rooms" :key="id">
-                <v-card-actions v-if="admin" class="d-flex flex-wrap w-100 justify-space-between">
-                    <v-btn color="yellow" variant="text"
-                        :to="`/events/${EventId}/edit/theme-room/${room.id}`">Editar</v-btn>
-                    <v-btn color="red" variant="text" @click="fetchDeleteRoom(room.id)">Exluir</v-btn>
-                </v-card-actions>
-                <v-card-title class="font-weight-bold text-h5 text-center">{{ room.name }}</v-card-title>
-                <v-card-subtitle class="text-caption grey--text">
-                    <v-icon class="mr-1" small>mdi-calendar</v-icon>
-                    De {{ room.start_date }} - Até {{ room.end_date }}
-                </v-card-subtitle>
-                <v-card-subtitle class="text-caption grey--text">
-                    <v-icon class="mr-1" small>mdi-clock-outline</v-icon>
-                    Horário de início {{ room.start_time }}
-                </v-card-subtitle>
-                <v-card-subtitle class="text-caption grey--text">
-                    <v-icon class="mr-1" small>mdi-map-marker-outline</v-icon>
-                    <span class="font-weight-bold">{{ room.local }}</span>
-                </v-card-subtitle>
-                <v-card-text>
-                    <v-row class="d-flex flex-column">
-                        <v-col>
-                            <p class="font-weight-bold">Palestrante: {{ room.speaker }}</p>
-                        </v-col>
-                        <v-col>
-                            <v-icon class="mr-1" small>mdi-information-outline</v-icon>
-                            <span class="font-weight-bold">Tipo: </span>
-                            {{ room.event_type }}
-                        </v-col>
-                        <v-col class="d-flex flex-wrap">
-                            <v-icon class="mr-1" small>mdi-account-group</v-icon>
-                            <p class="font-weight-bold">Participantes:</p>
-                            <p>Máximo {{ room.max_quantity }} - Mínimo {{ room.min_quantity }}
-                            </p>
-                        </v-col>
-                    </v-row>
-                </v-card-text>
-                <v-card-actions class="d-flex flex-wrap w-100 justify-space-between">
-                    <v-btn color="blue" text @click="room.showDetails = !room.showDetails">Ver detalhes</v-btn>
-                    <template v-if="isSubscribed">
-                        <v-btn @click="handleUnsubscription" color="error" class="text-no-wrap">
-                            <b>Cancelar Inscrição</b>
-                        </v-btn>
-                    </template>
-                    <v-btn v-else @click="handleSubscription" color="success" class="text-no-wrap">
-                        Participar
+<v-container fluid class="d-flex flex-column align-center">
+    <v-btn v-if="admin" :to="`/events/${EventId}/create/theme-room`" color="success">Cadastrar Sala Tematica
+    </v-btn>
+    <h2 v-if="rooms.length === 0">Nenhuma Sala Cadastrada ainda...</h2>
+    <v-container fluid class="d-flex flex-wrap ga-5 justify-center">
+        <v-card class="mx-2 my-2 justify-center w-100" variant="outlined" elevation="20" max-width="400"
+            min-width="50" v-for="(room, id) in rooms" :key="id">
+            <v-card-actions v-if="admin" class="d-flex flex-wrap w-100 justify-space-between">
+                <v-btn color="yellow" variant="text"
+                    :to="`/events/${EventId}/edit/theme-room/${room.id}`">Editar</v-btn>
+                <v-btn color="red" variant="text" @click="fetchDeleteRoom(room.id)">Exluir</v-btn>
+            </v-card-actions>
+            <v-card-title class="font-weight-bold text-h5 text-center">{{ room.name }}</v-card-title>
+            <v-card-subtitle class="text-caption grey--text">
+                <v-icon class="mr-1" small>mdi-calendar</v-icon>
+                De {{ room.start_date }} - Até {{ room.end_date }}
+            </v-card-subtitle>
+            <v-card-subtitle class="text-caption grey--text">
+                <v-icon class="mr-1" small>mdi-clock-outline</v-icon>
+                Horário de início {{ room.start_time }}
+            </v-card-subtitle>
+            <v-card-subtitle class="text-caption grey--text">
+                <v-icon class="mr-1" small>mdi-map-marker-outline</v-icon>
+                <span class="font-weight-bold">{{ room.local }}</span>
+            </v-card-subtitle>
+            <v-card-text>
+                <v-row class="d-flex flex-column">
+                    <v-col>
+                        <p class="font-weight-bold">Palestrante: {{ room.speaker }}</p>
+                    </v-col>
+                    <v-col>
+                        <v-icon class="mr-1" small>mdi-information-outline</v-icon>
+                        <span class="font-weight-bold">Tipo: </span>
+                        {{ room.event_type }}
+                    </v-col>
+                    <v-col class="d-flex flex-wrap">
+                        <v-icon class="mr-1" small>mdi-account-group</v-icon>
+                        <p class="font-weight-bold">Participantes:</p>
+                        <p>Máximo {{ room.max_quantity }} - Mínimo {{ room.min_quantity }}
+                        </p>
+                    </v-col>
+                </v-row>
+            </v-card-text>
+            <v-card-actions class="d-flex flex-wrap w-100 justify-space-between">
+                <v-btn color="blue" text @click="room.showDetails = !room.showDetails">Ver detalhes</v-btn>
+                <template v-if="isSubscribed">
+                    <v-btn @click="handleUnsubscription" color="error" class="text-no-wrap">
+                        <b>Cancelar Inscrição</b>
                     </v-btn>
-                </v-card-actions>
-                <v-expand-transition>
-                    <div v-show="room.showDetails">
-                        <v-divider></v-divider>
-                        <v-row class="pa-2">
-                            <v-card-text>
-                                Público Alvo: {{ room.audiences }}
-                            </v-card-text>
-                            <v-card-text>
-                                Descrição: {{ room.description }}
-                            </v-card-text>
-                        </v-row>
-                    </div>
-                </v-expand-transition>
-            </v-card>
-            <v-snackbar v-model="showSnackbar" :color="snackbarColor" timeout="4000" top>
-                {{ snackbarMessage }}
-            </v-snackbar>
-        </v-container>
+                </template>
+                <v-btn v-else @click="handleSubscription" color="success" class="text-no-wrap">
+                    Participar
+                </v-btn>
+            </v-card-actions>
+            <v-expand-transition>
+                <div v-show="room.showDetails">
+                    <v-divider></v-divider>
+                    <v-row class="pa-2">
+                        <v-card-text>
+                            Público Alvo: {{ room.audiences }}
+                        </v-card-text>
+                        <v-card-text>
+                            Descrição: {{ room.description }}
+                        </v-card-text>
+                    </v-row>
+                </div>
+            </v-expand-transition>
+        </v-card>
+        <v-snackbar v-model="showSnackbar" :color="snackbarColor" timeout="4000" top>
+            {{ snackbarMessage }}
+        </v-snackbar>
     </v-container>
+</v-container>
 </template>
 
 <script setup lang="ts">
