fa37d888b3d8d7132de1f30822394ab39a6079f4
mts-lucas <lmateus1067@outlook.com>
2024-08-21 13:36:56 -0300

    chore: implements organizer in certificate #73

--- frontend/eventsync_front/src/utils/certificate.ts
+++ frontend/eventsync_front/src/utils/certificate.ts
@@ -1,39 +1,55 @@
 import jsPDF from 'jspdf';
+import { getOrganizer } from '@/services/eventService';
+import { type Profile } from '@/types/users';
 
-export const gerarCertificado = (
+export const gerarCertificado = async (
     nomeUsuario: string,
     nomeEvento: string,
     horasEvento: number,
     dataEvento1: string,
     dataEvento2: string,
-    nomeOrg: string,
+    eventId: number,
 ) => {
-    const doc = new jsPDF();
+    try {
+        // Obtendo informações do organizador
+        const response = await getOrganizer(eventId);
+        const organizer: Profile = response.data;
 
-    // Definindo o título do certificado
-    doc.setFontSize(22);
-    doc.text('Certificado de Participação', 105, 30, { align: 'center' });
+        // Verificando se o organizador foi encontrado
+        if (!organizer) {
+            throw new Error('Organizador não encontrado');
+        }
 
-    // Adicionando texto do certificado
-    doc.setFontSize(16);
-    doc.text(
-        `Certificamos que ${nomeUsuario} participou do evento ${nomeEvento}`,
-        20,
-        60
-    );
-    doc.text(
-        `com duração de ${horasEvento} horas, realizado entre os dias ${dataEvento1} e ${dataEvento2}.`,
-        20,
-        75
-    );
+        const doc = new jsPDF();
 
-    // Assinatura
-    doc.text(`${nomeOrg}`, 105, 130, { align: 'center' });
-    doc.text('Nome do Organizador', 105, 140, { align: 'center' });
+        // Definindo o título do certificado
+        doc.setFontSize(22);
+        doc.text('Certificado de Participação', 105, 30, { align: 'center' });
 
-    // Nome do arquivo
-    const nomeArquivo = `Certificado_${nomeUsuario.replace(/\s+/g, '_')}.pdf`;
+        // Adicionando texto do certificado
+        doc.setFontSize(16);
+        doc.text(
+            `Certificamos que ${nomeUsuario} participou do evento ${nomeEvento}`,
+            20,
+            60
+        );
+        doc.text(
+            `com duração de ${horasEvento} horas, realizado entre os dias ${dataEvento1} e ${dataEvento2}.`,
+            20,
+            75
+        );
 
-    // Gerando o PDF e fazendo download
-    doc.save(nomeArquivo);
+        // Assinatura
+        doc.text(`${organizer.name}`, 105, 130, { align: 'center' });
+        doc.text('Organizador do Evento', 105, 140, { align: 'center' });
+
+        // Nome do arquivo
+        const nomeArquivo = `Certificado_${nomeUsuario.replace(/\s+/g, '_')}.pdf`;
+
+        // Gerando o PDF e fazendo download
+        doc.save(nomeArquivo);
+    } catch (error) {
+        console.error('Erro ao gerar o certificado:', error);
+        throw error;
+    }
 };
