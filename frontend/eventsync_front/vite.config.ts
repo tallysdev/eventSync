17de68fa2babb2a3ffce2ca02716b8dc7800af25
Vinicius Maia <viniciusmaiamarinho1@gmail.com>
2024-08-22 02:21:00 -0300

    build: update chunk size #77

--- frontend/eventsync_front/vite.config.ts
+++ frontend/eventsync_front/vite.config.ts
@@ -10,7 +10,21 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // Split large libraries into separate chunks
+          vue: ['vue'],
+          vuetify: ['vuetify'],  // If using Vuetify
+          axios: ['axios'],       // If axios is heavily used
+          // Additional libraries or components can be split here
+        },
+      },
+    },
+    chunkSizeWarningLimit: 500,  // Adjust this value based on your needs
+  },
+});
